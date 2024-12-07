9a0ad6dbf380b423489645762429aca52e996a7a
Daphne Simons <daphne.simons@devacademy.co.nz>
2024-12-08 11:38:20 +1300

    db functions - orderBy data by timestamp, now get result from earliest to latest.

--- backend/server/db/db.ts
+++ backend/server/db/db.ts
@@ -3,19 +3,25 @@ import db from './connection'
 
 // Query data from all time
 export async function getAllCows() {
-  return await db('device_positions').select('*')
+  return await db('device_positions')
+    .orderBy('utc_timestamp', 'asc')
+    .select('*')
 }
 
 // Query data at a specific timestamp
 export async function getAllCowsByTime(timestamp: string) {
   return await db('device_positions')
     .where('utc_timestamp', timestamp)
+    .orderBy('utc_timestamp', 'asc')
     .select('*')
 }
 
 // Query data for a specific cow
 export async function getCow(id: string) {
-  return await db('device_positions').where('cattle_name', id).select('*')
+  return await db('device_positions')
+    .where('cattle_name', id)
+    .orderBy('utc_timestamp', 'asc')
+    .select('*')
 }
 
 // Query data for a specific cow at a specific timestamp
@@ -23,5 +29,6 @@ export async function getCowByTime(id: string, timestamp: string) {
   return await db('device_positions')
     .where('cattle_name', id)
     .andWhere('utc_timestamp', timestamp)
+    .orderBy('utc_timestamp', 'asc')
     .select('*')
 }
